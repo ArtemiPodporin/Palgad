9cfad359e8e96d26041fd0fc35193ac17c225d40
ArtemiPodporin <120027758+ArtemiPodporin@users.noreply.github.com>
2023-02-26 15:19:47 +0200

    AS

--- MyModule.py
+++ MyModule.py
@@ -6,4 +6,11 @@ def calculate_net_salary(gross_salary, tax_rate):
 def print_net_salaries(names, salaries, tax_rate):
     for i in range(len(names)):
         net_salary = calculate_net_salary(salaries[i], tax_rate)
-        print(f"{names[i]}: Чистая зарплата = {net_salary}")
+        print(f"{names[i]}: netopalk = {net_salary}")
+
+#19
+from palgad import average_salary
+
+palgad = [1200, 2500, 750, 395, 1200]
+avg = average_salary(palgad)
+print("Средняя зарплата:", avg)
--- Palgad.py
+++ Palgad.py
@@ -2,13 +2,22 @@
 def calculate_salary(gross_salary, tax_rate):
     net_salary = gross_salary - (gross_salary * tax_rate / 100)
     return net_salary
-
 palgad = [1200, 2500, 750, 395, 1200]
 inimesed = ["A", "B", "C", "D", "E"]
-
 tax_rate = 13 
 for i in range(len(palgad)):
     net_salary = calculate_salary(palgad[i], tax_rate)
     print(f"{inimesed[i]}: netopalk = {net_salary}")
 
 #19
+from palgad import salary_increase
+
+palgad = [1200, 2500, 750, 395, 1200]
+inimesed = ["A", "B", "C", "D", "E"]
+
+print("Изначальный список зарплат:", palgad)
+
+percent = int(input("Введите процент увеличения зарплаты: "))
+new_palga = salary_increase(palgad, inimesed, percent)
+
+print("Обновленный список зарплат:", new_palga)
